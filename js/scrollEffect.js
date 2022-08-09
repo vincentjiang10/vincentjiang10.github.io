fd32ccaaa978eadd99eb8193db58b19add06ff92
Vincent Jiang <vincentjiang2003@gmail.com>
2022-08-08 20:11:01 -0400

    Fix scrolling effects and add jQuery

--- js/scrollEffect.js
+++ js/scrollEffect.js
@@ -1,18 +1,31 @@
-const contentItems = document.querySelector(".contentItem");
+// -- Scrolling animation ---
 
-/*
+const contentItems = document.querySelectorAll(".contentItem");
+
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    emtry.target.classList.toggle("show", entry.isIntersecting);
+    entry.target.classList.toggle("show", entry.isIntersecting);
   });
+}, {
+  threshold: 0.5,
 });
 
+// set observer to observe each contentItem
 contentItems.forEach(contentItem => {
   observer.observe(contentItem);
-})*/
+});
 
-const observer = new IntersectionObserver(entries => {
-  console.log(entries);
+// -- Remove anchor link in URL when clicked ---
+
+// when the DOM has fully loaded
+$(document).ready(() => {
+  const anchor = $(".sidenav div");
+  // when clicked
+  anchor.click(() => {
+    setTimeout(() => { removeHash(); }, 10);
+  });
 })
 
-observer.observe(contentItems[0]);
+const removeHash = () => {
+  history.replaceState('', document.title, `${window.location.origin}${window.location.pathname}${window.location.search}`);
+}
