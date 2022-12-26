75beaea41f3268c05ad8dc02415f28693935084c
Vincent Jiang <vj68@cornell.edu>
2022-12-25 23:43:25 -0500

    Add dragging animation to theme background

--- js/theme.js
+++ js/theme.js
@@ -2,11 +2,9 @@
  * Theme background animation and event logic
  */
 
-// Initialize the position of the background image
-let x = 0;
-
 // Initialize the mouse position
 let mouseX = 0;
+let mouseY = 0;
 
 // Flag to track whether the mouse is down or not
 let isMouseDown = false;
@@ -14,65 +12,148 @@ let isMouseDown = false;
 // Get the element that will be dragged
 const theme = document.querySelector('.theme');
 
-// Affects animtion attributes of the children of theme
+// Current Animation
+let currentAnimations = [];
+
+// Mouse event snapshot
+let snapshot = {
+  x: 0,
+  y: 0,
+  time: 0,
+}
+
+// Affects animation attributes of the children of theme
 function changeAnimation(callback) {
+  // Clear animation
+  currentAnimations = [];
   const children = theme.children;
   Array.from(children).forEach(callback);
 }
 
+// Pause animation
+function pauseAnimation() {
+  currentAnimations.forEach((animation) => animation.pause());
+}
+
+// Resume animation
+function playAnimation() {
+  currentAnimations.forEach((animation) => animation.play());
+}
+
 // Add initial animation
 changeAnimation((child) => {
-  child.animate([
-    { backgroundPosition: `${x}px 0` },
-    { backgroundPosition: `${x + 500}px 0` },
+  const animation = child.animate([
+    { backgroundPosition: `0 0` },
+    { backgroundPosition: `5000px 0` },
   ], {
-    duration: 10000,
-    iterations: Infinity
-  })
-});
+    duration: 100000,
+    iterations: Infinity,
+  });
+  currentAnimations.push(animation);
+})
 
-// Change cursor 
-theme.addEventListener('mouseenter', function () {
-  theme.style.cursor = 'grab';
-});
 
-theme.addEventListener('mouseleave', function () {
-  theme.style.cursor = 'pointer';
-});
+// Add event listeners to theme
+theme.addEventListener('mouseenter', function() {
+  // Change cursor
+  theme.style.cursor = "grab";
+})
+theme.addEventListener('mouseleave', event => {
+  // Change cursor
+  theme.style.cursor = "pointer";
 
-// Add event listeners to `theme`
+  // Play animation
+  playAnimation();
+
+  // Mouse is down
+  isMouseDown = false;
+})
 theme.addEventListener('mousedown', (event) => {
   // Set the mouse position
   mouseX = event.clientX;
+  mouseY = event.clientY;
+
+  // Pause animaiton
+  pauseAnimation();
 
   // Set the flag to true
-  isMouseDown = true;
+  handleMouseDown(event);
 });
 theme.addEventListener('mousemove', (event) => {
-  // Check if the mouse is down
-  if (isMouseDown) {
-    // Calculate the difference between the current and previous mouse positions
-    const dx = event.clientX - mouseX;
+  // If mouse is not down
+  if (!isMouseDown) return;
+  
+  // Calculate the difference between the current and previous mouse positions
+  let dx = event.clientX - mouseX;
+  let dy = event.clientY - mouseY;
 
-    // Update the position of the background image
-    x += dx;
+  // Update animation
+  changeAnimation((child) => {
+    // TODO: See if you can remove this part
+    const computedStyle = window.getComputedStyle(child);
+    const backgroundPosition = computedStyle.getPropertyValue('background-position');
 
-    // Update animation
-    changeAnimation((child) => {
-      child.animate([
-        { backgroundPosition: `${x}px 0` },
-        { backgroundPosition: `${x + 5000}px 0` },
-      ], {
-        duration: 100000,
-        iterations: Infinity
-      })
+    const [x, y] = backgroundPosition.split(' ').map(value => parseInt(value, 10));
+    
+    const animation = child.animate([
+      { backgroundPosition: `${dx + x}px ${dy + y}px` },
+      { backgroundPosition: `${dx + x + 5000}px ${dy + y}px` },
+    ], {
+      duration: 100000,
+      iterations: Infinity,
     });
+    currentAnimations.push(animation);
+  });
 
-    // Update the mouse position
-    mouseX = event.clientX;
-  }
+  // Pause animation
+  pauseAnimation();
+
+  // Update the mouse position
+  mouseX = event.clientX;
+  mouseY = event.clientY;
 });
-theme.addEventListener('mouseup', function () {
+theme.addEventListener('mouseup', (event) => {
+  // Resume animation
+  playAnimation();
+
   // Set the flag to false
-  isMouseDown = false;
+  handleMouseUp(event);
 });
+
+// Mouse down and up handlers
+function handleMouseDown(event) {
+  isMouseDown = true;
+  // Snapshot
+  snapshot = {
+    x: event.clientX,
+    y: event.clientY,
+    time: event.timeStamp
+  }
+}
+function handleMouseUp(event) {
+  isMouseDown = false;
+  const {x, y, time} = snapshot;
+  const dx = event.clientX - x;
+  const dy = event.clientY - y;
+  const dt = event.timeStamp - time;
+
+  // Find speed
+  const dist = Math.sqrt(dx ** 2 + dy ** 2);
+  const speed = Math.min(0.25, dist / dt);
+
+  // Update animation
+  changeAnimation((child) => {
+    const computedStyle = window.getComputedStyle(child);
+    const backgroundPosition = computedStyle.getPropertyValue('background-position');
+
+    const [x, y] = backgroundPosition.split(' ').map(value => parseInt(value, 10));
+    const animation = child.animate([
+      { backgroundPosition: `${x}px ${y}px` },
+      { backgroundPosition: `${x + 5000 * dx / dist}px ${y + 5000 * dy / dist}px` },
+    ], {
+      duration: 5000 / speed,
+      iterations: Infinity,
+    });
+    currentAnimations.push(animation);
+  });
+}
