c761fe8f3b4fbf5845aedbcfc64d42414a023da5
Vincent Jiang <vj68@cornell.edu>
2023-05-10 20:24:24 -0400

    Update resume

--- js/content.js
+++ js/content.js
@@ -7,7 +7,7 @@ const Content = {
     head: "Roblox",
     subHead1: "Software Engineer Intern",
     subHead2: "Summer 2023",
-    bodyText: "Coming Soon!"
+    bodyText: "Coming Soon!",
   },
 
   YOOMI: {
@@ -18,9 +18,10 @@ const Content = {
     head: "Yoomi",
     subHead1: "Software Engineer Intern",
     subHead2: "August 2022 &#8212; Present",
-    bodyText: "Refactor and develop code in TypeScript responsible for motion-tracking behavior, \
+    bodyText:
+      "Refactor and develop code in TypeScript responsible for motion-tracking behavior, \
                data collection, and animations on HTML5 Canvas to provide an interactive \
-               exercising experience for in-patients in physical therapy."
+               exercising experience for in-patients in physical therapy.",
   },
 
   CUP: {
@@ -31,8 +32,9 @@ const Content = {
     head: "Cornell Cup Robotics",
     subHead1: "Software Engineer",
     subHead2: "February 2022 &#8212; January 2023",
-    bodyText: "Translate user speech inputs into text using machine learning and natural language \
-               processing as part of the CS Chatbot team."
+    bodyText:
+      "Translate user speech inputs into text using machine learning and natural language \
+               processing as part of the CS Chatbot team.",
   },
 
   CIS: {
@@ -43,8 +45,9 @@ const Content = {
     head: "Cornell Bowers CIS",
     subHead1: "Course Consultant",
     subHead2: "August 2022 &#8212; Present",
-    bodyText: "Facilitate Object-Oriented Programming and Data Structures (CS 2110) discussion \
-               sections and assist students in course understanding, assignments, and software."
+    bodyText:
+      "Facilitate Object-Oriented Programming and Data Structures (CS 2110) discussion \
+               sections and assist students in course understanding, assignments, and software.",
   },
 
   CORNELL_ENG: {
@@ -55,8 +58,9 @@ const Content = {
     head: "Cornell Engineering",
     subHead1: "Computer Science and Mathematics Tutor",
     subHead2: "January 2022 &#8212; Present",
-    bodyText: "Tutor and advise students in Object-Oriented Programming (CS 2110) and \
-               Multivariable Calculus (MATH 1920) under the Engineering Learning Initiatives Program."
+    bodyText:
+      "Tutor and advise students in Object-Oriented Programming (CS 2110) and \
+               Multivariable Calculus (MATH 1920) under the Engineering Learning Initiatives Program.",
   },
 
   PAC_CAMEL: {
@@ -68,9 +72,15 @@ const Content = {
     head: "Pac-Camel",
     subHead1: "OCaml, Bogue, SDL",
     subHead2: "September 2022 &#8212; December 2022",
-    buttons: [{href: "https://github.com/vjiang10/pac-camel", icon: "fa-github-square"}],
-    bodyText: "Camel version of Pac-Man. Features include random map generation, controls and movements, \
-               power-ups and items."
+    buttons: [
+      {
+        href: "https://github.com/vjiang10/pac-camel",
+        icon: "fa-github-square",
+      },
+    ],
+    bodyText:
+      "Camel version of Pac-Man. Features include random map generation, controls and movements, \
+               power-ups and items.",
   },
 
   TOWERS_OF_HANOI: {
@@ -82,10 +92,19 @@ const Content = {
     head: "Towers of Hanoi",
     subHead1: "JavaScript, HTML, CSS, React.js, Three.js, Firebase",
     subHead2: "July 2022 &#8212; August 2022",
-    buttons: [{href: "https://towers-of-hanoi-4d72b.web.app/", icon: "fa-external-link-square"}, 
-              {href: "https://github.com/vjiang10/towers-of-hanoi", icon: "fa-github-square"}],
-    bodyText: "Web app and game based on the Towers of Hanoi math puzzle. \
-               The app contains three different puzzle types: Standard, Bicolor, and Adjacent."
+    buttons: [
+      {
+        href: "https://towers-of-hanoi-4d72b.web.app/",
+        icon: "fa-external-link-square",
+      },
+      {
+        href: "https://github.com/vjiang10/towers-of-hanoi",
+        icon: "fa-github-square",
+      },
+    ],
+    bodyText:
+      "Web app and game based on the Towers of Hanoi math puzzle. \
+               The app contains three different puzzle types: Standard, Bicolor, and Adjacent.",
   },
 
   TERRAIN_MAP: {
@@ -97,9 +116,15 @@ const Content = {
     head: "Terrain Map",
     subHead1: "Java, Swing, AWT",
     subHead2: "June 2022 &#8212; July 2022",
-    buttons: [{href: "https://github.com/vjiang10/terrain-map", icon: "fa-github-square"}],
-    bodyText: "3D visualization tool for popular stochastic algorithms for fractal landscape generation \
-               that model Brownian motion, including Midpoint Displacement, Diamond Square, and Perlin Noise."
+    buttons: [
+      {
+        href: "https://github.com/vjiang10/terrain-map",
+        icon: "fa-github-square",
+      },
+    ],
+    bodyText:
+      "3D visualization tool for popular stochastic algorithms for fractal landscape generation \
+               that model Brownian motion, including Midpoint Displacement, Diamond Square, and Perlin Noise.",
   },
 
   CHOICE_GAME: {
@@ -111,8 +136,14 @@ const Content = {
     head: "Choice Game",
     subHead1: "C, ImageMagick",
     subHead2: "January 2021 &#8212; February 2021",
-    buttons: [{href: "https://github.com/vjiang10/choice-game", icon: "fa-github-square"}],
-    bodyText: "A choice-based game run on the terminal that relies on user command line inputs. \
-               A horror-themed story plays out with the aid of pictures depending on the user's decisions."
-  }
-}
+    buttons: [
+      {
+        href: "https://github.com/vjiang10/choice-game",
+        icon: "fa-github-square",
+      },
+    ],
+    bodyText:
+      "A choice-based game run on the terminal that relies on user command line inputs. \
+               A horror-themed story plays out with the aid of pictures depending on the user's decisions.",
+  },
+};
